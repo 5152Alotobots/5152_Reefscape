f6b6ddf3331cd630648950c76fa0eb7534b10be7
general <alotobots5152@gmail.com>
2025-02-12 22:00:04 -0800

    io update

--- src/main/java/frc/alotobots/reefscape/subsystems/coralIntake/io/CoralIntakeIO.java
+++ src/main/java/frc/alotobots/reefscape/subsystems/coralIntake/io/CoralIntakeIO.java
@@ -2,9 +2,18 @@ package frc.alotobots.reefscape.subsystems.coralIntake.io;
 
 import org.littletonrobotics.junction.AutoLog;
 
+
+import static edu.wpi.first.units.Units.*;
+
+import edu.wpi.first.units.measure.*;
+
 public interface CoralIntakeIO {
     @AutoLog
     public static class CoralIntakeIOInputs {
+        public boolean motorConnected = false;
         
+        public LinearVelocity Velocity = MetersPerSecond.zero();
+        public Voltage motorAppliedVolts = Volts.zero();
+        public Current motorCurrentAmps = Amps.zero();
     }
 }
