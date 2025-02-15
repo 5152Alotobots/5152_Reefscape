5b03f9d50f73128d363dec0ff933a9048f13c70b
rkortlandt <rowankortlandt@otsegops.org>
2025-02-15 15:42:44 -0300

    additional changes

--- src/main/java/frc/alotobots/reefscape/subsystems/coralIntake/commands/CoralIntakeDefault.java
+++ src/main/java/frc/alotobots/reefscape/subsystems/coralIntake/commands/CoralIntakeDefault.java
@@ -1,7 +1,15 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
+/*
+* ALOTOBOTS - FRC Team 5152
+  https://github.com/5152Alotobots
+* Copyright (C) 2025 ALOTOBOTS
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Source code must be publicly available on GitHub or an alternative web accessible site
+*/
 package frc.alotobots.reefscape.subsystems.coralIntake.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
