cf8805afb860b9970c61616f22b302de1e875aa5
alanmeye <alotobots5152@gmail.com>
2025-03-01 08:12:01 -0800

    Update at 'Sat Mar 01 08:12:01 PST 2025'

--- src/main/java/frc/alotobots/reefscape/subsystems/elevator/constants/ElevatorConstants.java
+++ src/main/java/frc/alotobots/reefscape/subsystems/elevator/constants/ElevatorConstants.java
@@ -64,7 +64,8 @@ public final class ElevatorConstants {
     public static final Distance CLIMB = Meters.of(1.1);
 
     /** Height for picking up from coral station */
-    public static final Distance CORAL_CORAL_STATION = Meters.of(0.75);
+    public static final Distance CORAL_CORAL_STATION = Meters.of(0.82
+    );
 
     /** Height for L4 placement */
     public static final Distance CORAL_L4_PLACE = Meters.of(1.78);
