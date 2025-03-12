1fab8fb246598fbe048496a8a58ca4288b9b55f5
rkortlandt <rowankortlandt@otsegops.org>
2025-03-11 22:33:45 -0500

    update

--- src/main/java/frc/alotobots/RobotContainer.java
+++ src/main/java/frc/alotobots/RobotContainer.java
@@ -46,6 +46,8 @@ import frc.alotobots.library.subsystems.vision.photonvision.apriltag.util.AprilT
 import frc.alotobots.library.subsystems.vision.photonvision.objectdetection.io.*;
 import frc.alotobots.reefscape.commands.groups.Climb;
 import frc.alotobots.reefscape.commands.groups.UnClimb;
+import frc.alotobots.reefscape.commands.states.algae.StateAlgaeRemoveL2;
+import frc.alotobots.reefscape.commands.states.algae.StateAlgaeRemoveL3;
 import frc.alotobots.reefscape.commands.states.coral.*;
 import frc.alotobots.reefscape.subsystems.climber.ClimberSubsystem;
 import frc.alotobots.reefscape.subsystems.climber.commands.ClimberDisableServos;
@@ -257,7 +259,10 @@ public class RobotContainer {
 
     stateCoralCoralStationButton.toggleOnTrue(
         new StateCoralCoralStation(
-            elevatorSubsystem, wristSubsystem, coralIntakeSubsystem, blingSubsystem));
+            elevatorSubsystem, 
+            wristSubsystem, 
+            coralIntakeSubsystem, 
+            blingSubsystem));
     stateCoralL1Button.toggleOnTrue(
         new StateCoralL1(
             elevatorSubsystem,
@@ -287,10 +292,22 @@ public class RobotContainer {
             blingSubsystem,
             coralIntakeReleaseButton));
     stateCoralStowedButton.toggleOnTrue(new StateCoralStowed(elevatorSubsystem, wristSubsystem));
-
     stateCoralGroundButton.toggleOnTrue(
         new StateCoralGround(
             elevatorSubsystem, wristSubsystem, coralIntakeSubsystem, blingSubsystem));
+
+    stateAlgaeL2Button.toggleOnTrue(new StateAlgaeRemoveL2(
+        elevatorSubsystem, 
+        wristSubsystem, 
+        coralIntakeSubsystem, 
+        blingSubsystem, 
+        coralIntakeReleaseButton));
+    stateAlgaeL3Button.toggleOnTrue(new StateAlgaeRemoveL3(
+        elevatorSubsystem, 
+        wristSubsystem, 
+        coralIntakeSubsystem, 
+        blingSubsystem, 
+        coralIntakeReleaseButton));
 
     climbButton.toggleOnTrue(
         new Climb(climberSubsystem, elevatorSubsystem, blingSubsystem, () -> -getElevatorAxis()));
