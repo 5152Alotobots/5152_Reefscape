b8577fe11eed8eea6510e350c2fd84082e7f3924
FunkyChicken <sean.ernstes@proton.me>
2025-03-13 22:16:52 -0400

    fix do not update rotation

--- src/main/java/frc/alotobots/library/subsystems/vision/photonvision/apriltag/constants/AprilTagConstants.java
+++ src/main/java/frc/alotobots/library/subsystems/vision/photonvision/apriltag/constants/AprilTagConstants.java
@@ -55,7 +55,7 @@ public class AprilTagConstants {
   // (Adjusted automatically based on distance and # of tags)
   // LOWER = TRUST MORE
   public static double MULTI_TAG_LINEAR_STD_DEV_BASE = 0.02; // Meters
-  public static double MULTI_TAG_ANGULAR_STD_DEV_BASE = 0.06; // Radians
+  public static double MULTI_TAG_ANGULAR_STD_DEV_BASE = 1000; // Radians
 
   public static double SINGLE_TAG_LINEAR_STD_DEV_BASE = 0.06; // Meters
   public static double SINGLE_TAG_ANGULAR_STD_DEV_BASE =
