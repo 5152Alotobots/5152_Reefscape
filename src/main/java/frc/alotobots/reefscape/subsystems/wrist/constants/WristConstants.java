8b5d8d2d3cf4db39282e96d4c31a0ff573e397d1
alanmeye <alotobots5152@gmail.com>
2025-04-03 19:13:38 -0400

    Update at 'Thu Apr 03 19:13:38 EDT 2025'

--- src/main/java/frc/alotobots/reefscape/subsystems/wrist/constants/WristConstants.java
+++ src/main/java/frc/alotobots/reefscape/subsystems/wrist/constants/WristConstants.java
@@ -67,7 +67,7 @@ public class WristConstants {
     public static final Angle CORAL_L4_PLACE = Degrees.of(90);
 
     /** Angle for L3 placement (Equal to the angle for L2 placement) */
-    public static final Angle CORAL_L3_PLACE = Degrees.of(118);
+    public static final Angle CORAL_L3_PLACE = Degrees.of(104);
 
     /** Angle for L2 placement (Equal to the angle for L3 placement) */
     public static final Angle CORAL_L2_PLACE = Degrees.of(104);
